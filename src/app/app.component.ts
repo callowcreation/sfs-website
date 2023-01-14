88aad3e4f55134e7c931c6ca6255d93e7649a337
Jones Cropper <jones.cropper@quantiphi.com>
2023-01-14 14:56:19 -0800

    removed unnecessary code and files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sfs-website';
+    title = 'sfs-website';
+
+    loading: boolean = true;
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.loading = false;
+        }, 1000);
+    }
 }
