96051106aa50460a7d562b9b7c4c4ffd1616ae41
Jones Cropper <jones.cropper@quantiphi.com>
2023-01-28 13:46:30 -0800

    View current shoutouts on dashboard page

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { map } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
 import { ConfigurationService } from 'src/app/services/configuration.service';
@@ -13,22 +14,38 @@ export class DashboardComponent {
 
     pinned: string = 'streamer0';
     guests: any[] = [
-        { login: 'streamer1', posted: { login: 'woLLac', timestamp: 1607583840213 } },
-        { login: 'streamer2', posted: { login: 'naivebot', timestamp: 1604538820851 } },
-        { login: 'streamer3', posted: { login: 'callowcreation', timestamp: 1590623631617 } },
-        { login: 'streamer4', posted: { login: 'sfs', timestamp: 1589578178831 } },
+        { login: 'streamer1', profile_image_url: '', posted: { login: 'woLLac', timestamp: 1607583840213 } },
+        { login: 'streamer2', profile_image_url: '', posted: { login: 'naivebot', timestamp: 1604538820851 } },
+        { login: 'streamer3', profile_image_url: '', posted: { login: 'callowcreation', timestamp: 1590623631617 } },
+        { login: 'streamer4', profile_image_url: '', posted: { login: 'sfs', timestamp: 1589578178831 } },
     ];
 
-    constructor(private storage: StorageService, configuration: ConfigurationService, private backend: BackendService) {
+    form = new FormGroup({
+        guests: new FormControl(this.guests, [Validators.required])
+    });
 
+    validator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            return null;
+        }
+    }
+
+    constructor(storage: StorageService, private backend: BackendService) {
         this.backend.get<any>(`/v3/api/dashboard/${storage.user?.id}`)
-        .pipe(map(({guests}) => {
+            .pipe(map(({ guests }) => {
+                return {
+                    guests: guests.map((x: any) => ({ login: x.login, profile_image_url: x.profile_image_url, posted: { login: x.posted.login, timestamp: x.posted.timestamp } }))
+                };
+            }))
+            .subscribe(({ guests }) => {
+                console.log({ guests });
+                this.guests = guests;
+                this.form.setValue({ guests });
+            });
+            this.form.reset();
+    }
 
-            return {guests};
-        }))
-        .subscribe(({ guests }) => {
-            console.log({ guests });
-            this.guests = guests;
-        });
+    onSubmit() {
+        console.log(this.form.value);
     }
 }
