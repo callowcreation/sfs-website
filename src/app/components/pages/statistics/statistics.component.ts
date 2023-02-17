ccba43d7ede65a107b17068fef952f5e8d314383
caLLowCreation <callowcreation@gmail.com>
2023-02-17 12:09:08 -0800

    Refactored methods DRY

--- src/app/components/pages/statistics/statistics.component.ts
+++ src/app/components/pages/statistics/statistics.component.ts
@@ -12,7 +12,7 @@ export interface Dessert {
     protein: number;
 }
 
-interface Statistics {
+interface Statistics extends Record<string, any> {
     guests: any[];
     posted_bys: any[];
     firsts: any[];
@@ -27,17 +27,14 @@ interface Statistics {
 export class StatisticsComponent {
 
     statistics: Statistics;
-    sortedGuests: any[] = [];
-    sortedPostedBys: any[] = [];
-    sortedFirsts: any[] = [];
-    sortedRecents: any[] = [];
+    sorted: Statistics;
 
     @ViewChild(MatTabGroup) tabGroup!: MatTabGroup;
 
     length = 50;
     pageSize = 10;
     pageIndex = 0;
-    pageSizeOptions = [5, 10, 25];
+    pageSizeOptions = [5, 10, 25, 50];
 
     hidePageSize = false;
     showPageSizeOptions = true;
@@ -49,6 +46,7 @@ export class StatisticsComponent {
     constructor(private storage: StorageService, private backend: BackendService) {
 
         this.statistics = { guests: [], posted_bys: [], firsts: [], recents: [] };
+        this.sorted = { guests: [], posted_bys: [], firsts: [], recents: [] };
 
         this.backend.get<any>(`/v3/api/common/${this.storage.user?.id}`, {
             statistics: true, // or object defining what parts of the [guests or any property, ie. features...] to return
@@ -56,83 +54,33 @@ export class StatisticsComponent {
             .subscribe(({ statistics }) => {
                 this.statistics = statistics;
                 console.log({ statistics });
-                this.sortedGuests = this.statistics.guests.slice(0, this.pageSize);//.sort((a, b) => compare(a.total, b.total, false));
-                this.sortedPostedBys = this.statistics.posted_bys.slice(0, this.pageSize);//.sort((a, b) => compare(a.total, b.total, false));
-                this.sortedFirsts = this.statistics.firsts.slice(0, this.pageSize);//.sort((a, b) => compare(a.timestamp, b.timestamp, true));
-                this.sortedRecents = this.statistics.recents.slice(0, this.pageSize);//.sort((a, b) => compare(a.timestamp, b.timestamp, false));
 
                 this.length = this.statistics.guests.length;
-
+                this.sorted = { 
+                    guests: this.statistics.guests.slice(0, this.pageSize), 
+                    posted_bys: this.statistics.posted_bys.slice(0, this.pageSize), 
+                    firsts: this.statistics.firsts.slice(0, this.pageSize), 
+                    recents: this.statistics.recents.slice(0, this.pageSize) 
+                };
             });
     }
 
-    sortShoutouts(sort: any): void {
-        const data = this.statistics.guests.slice();
+    sortTab(sort: any, key: string): void {
+        const data = this.statistics[key].slice();
 
-        if (firstInit(this.sortedGuests, sort, data)) return;
+        if (firstInit(this.sorted[key], sort, data)) return;
 
-        this.statistics.guests = data.sort((a, b) => {
+        this.statistics[key] = data.sort((a: any, b: any) => {
             return compare(a[sort.active], b[sort.active], sort.direction === 'asc');
         });
 
-        this.sortedGuests = this.pageSlice(this.statistics.guests);
-    }
-
-    sortPostedBys(sort: any): void {
-        const data = this.statistics.posted_bys.slice();
-
-        if (firstInit(this.sortedPostedBys, sort, data)) return;
-
-        this.statistics.posted_bys = data.sort((a, b) => {
-            return compare(a[sort.active], b[sort.active], sort.direction === 'asc');
-        });
-
-        this.sortedPostedBys = this.pageSlice(this.statistics.posted_bys);
-    }
-
-    sortFirsts(sort: any): void {
-        const data = this.statistics.firsts.slice();
-
-        if (firstInit(this.sortedFirsts, sort, data)) return;
-
-        this.statistics.firsts = data.sort((a, b) => {
-            return compare(a[sort.active], b[sort.active], sort.direction === 'asc');
-        });
-
-        this.sortedFirsts = this.pageSlice(this.statistics.firsts);
-    }
-
-    sortRecents(sort: any): void {
-        const data = this.statistics.recents.slice();
-
-        if (firstInit(this.sortedRecents, sort, data)) return;
-
-        this.statistics.recents = data.sort((a, b) => {
-            return compare(a[sort.active], b[sort.active], sort.direction === 'asc');
-        });
-
-        this.sortedRecents = this.pageSlice(this.statistics.recents);
+        this.sorted[key] = this.pageSlice(this.statistics[key]);
     }
 
     tabChanged(tabChangeEvent: MatTabChangeEvent): void {
-        switch (tabChangeEvent.index) {
-            case 0:
-                this.sortedGuests = this.tabSortedReset(this.statistics.guests);
-                break;
-            case 1:
-                this.sortedPostedBys = this.tabSortedReset(this.statistics.posted_bys);
-                break;
-            case 2:
-                this.sortedFirsts = this.tabSortedReset(this.statistics.firsts);
-                break;
-            case 3:
-                this.sortedRecents = this.tabSortedReset(this.statistics.recents);
-                break;
-            default:
-                this.pageIndex = 0;
-                this.length = 0;
-                break;
-        }
+        const keys = Object.keys(this.statistics);
+        const key = keys[tabChangeEvent.index];
+        this.sorted[key] = this.tabSortedReset(this.statistics[key]);
     }
 
     tabSortedReset(statistic: any[]): any[] {
@@ -155,22 +103,10 @@ export class StatisticsComponent {
         this.length = e.length;
         this.pageSize = e.pageSize;
         this.pageIndex = e.pageIndex;
-        switch (this.tabGroup.selectedIndex) {
-            case 0:
-                this.sortedGuests = this.pageSlice(this.statistics.guests);
-                break;
-            case 1:
-                this.sortedPostedBys = this.pageSlice(this.statistics.posted_bys);
-                break;
-            case 2:
-                this.sortedFirsts = this.pageSlice(this.statistics.firsts);
-                break;
-            case 3:
-                this.sortedRecents = this.pageSlice(this.statistics.recents);
-                break;
-            default:
-                break;
-        }
+
+        const keys = Object.keys(this.statistics);   
+        const key = keys[this.tabGroup.selectedIndex || 0];
+        this.sorted[key] = this.pageSlice(this.statistics[key]);
     }
 
     setPageSizeOptions(setPageSizeOptionsInput: string): void {
